8fabf1c007ea11b9822d099a134615b27d79005b
Neil McKimm <neil.mckimm@ucdconnect.ie>
2020-02-13 22:48:07 +0000

    changed API call to include a limit for the number of records to be returned per page as the default is 30 and there are 174 repositories on the scrapinghub github. I set it to 200 for very basic future future proofing for the duration of this trial

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ import requests
 
 app = Flask(__name__)
 
+access_token = '2bbadeac48f56d418461b1f27391c5c72e48081a'
+
 
 @app.route('/', methods=['GET'])
 def git_request():
-    repo_list = requests.get("https://api.github.com/users/scrapinghub/repos")
+    repo_list = requests.get("https://api.github.com/users/scrapinghub/repos?page=1&per_page=200")
     repo_list = repo_list.json()
-    print(repo_list)
+    print("number of items",len(repo_list))
+    for repo_list_item in repo_list:
+        print(repo_list_item['html_url'])
+        print(repo_list_item['stargazers_count'])
     return '<p> {} </p>'.format(repo_list)
 
 

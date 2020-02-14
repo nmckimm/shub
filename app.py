d0930e3c35ab80b3d786d4737d6dd40265e7a7f0
Neil McKimm <neil.mckimm@ucdconnect.ie>
2020-02-14 13:38:49 +0000

    Moved main code block out of the home function. Created three arrays: project_name, project_url, and stargazers_count in preparation to order the results shown in the html template from highest to lowest star rating. Added in print statements to ensure i have the same number of results

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
-access_token = '2bbadeac48f56d418461b1f27391c5c72e48081a'
+repo_list = requests.get("https://api.github.com/users/scrapinghub/repos?page=1&per_page=100")
+repo_list2 = requests.get("https://api.github.com/users/scrapinghub/repos?page=2&per_page=100")
+
+repo_list = repo_list.json()
+repo_list2 = repo_list2.json()
+print("number of items list 1:", len(repo_list))
+print("number of items list 2:", len(repo_list2))
+project_name = []
+project_url = []
+stargazers_count = []
+
+# for loops to add the project name, url, and star count to individual arrays which can be then sorted
+for repo_list_item in repo_list:
+    project_name.append(repo_list_item['name'])
+    project_url.append(repo_list_item['html_url'])
+    stargazers_count.append(repo_list_item['stargazers_count'])
+
+for repo_list_item in repo_list2:
+    project_name.append(repo_list_item['name'])
+    project_url.append(repo_list_item['html_url'])
+    stargazers_count.append(repo_list_item['stargazers_count'])
+
+print("\nNumber of project names: ", len(project_name))
+print("Number of  stargazers counts: ", len(stargazers_count))
+print("Number of project urls: ", len(project_url))
 
 
 @app.route('/', methods=['GET'])
-def git_request():
-    repo_list = requests.get("https://api.github.com/users/scrapinghub/repos?page=1&per_page=200")
-    repo_list = repo_list.json()
-    print("number of items",len(repo_list))
-    for repo_list_item in repo_list:
-        print(repo_list_item['html_url'])
-        print(repo_list_item['stargazers_count'])
-    return '<p> {} </p>'.format(repo_list)
+def home():
 
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()

f6e89051fd5a1287fa9dcdf30cde568a4fd0e144
Neil McKimm <neil.mckimm@ucdconnect.ie>
2020-02-14 18:13:34 +0000

    Tidied up python code and included try/except to catch any exceptions when making requests with the requests module. Removed print statements I used for tracking and included more comments for the code.

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
-# implement try catch with exception
-repo_list = requests.get("https://api.github.com/users/scrapinghub/repos?page=1&per_page=100")
-repo_list2 = requests.get("https://api.github.com/users/scrapinghub/repos?page=2&per_page=100")
+
+try:
+    repo_list = requests.get("https://api.github.com/users/scrapinghub/repos?page=1&per_page=100")
+except requests.exceptions.RequestException as e:
+    print(f"Could not make API call due to exception: {e}")
+try:
+    repo_list2 = requests.get("https://api.github.com/users/scrapinghub/repos?page=2&per_page=100")
+except requests.exceptions.RequestException as e:
+    print(f"Could not make API call due to exception: {e}")
 
 repo_list = repo_list.json()
 repo_list2 = repo_list2.json()
-print("number of items list 1:", len(repo_list))
-print("number of items list 2:", len(repo_list2))
+
+# Creating arrays to hold the star count, project name, and project urls individually in order to use the bubble sort algorithm
+# I am aware I could use in-built functions, I just wanted to show that I have worked with sorting algorithms also
 project_name = []
 project_url = []
 stargazers_count = []
@@ -25,13 +32,12 @@ for repo_list_item in repo_list2:
     project_url.append(repo_list_item['html_url'])
     stargazers_count.append(repo_list_item['stargazers_count'])
 
-print("\nNumber of project names: ", len(project_name))
-print("Number of  stargazers counts: ", len(stargazers_count))
-print("Number of project urls: ", len(project_url))
-
 
 def bubble_sort(stargazers_count, project_name, project_url):
-    # function to sort the three arrays
+    """
+    function to sort the three arrays
+
+    """
     is_swapped = True
     while is_swapped:
         is_swapped = False
@@ -48,7 +54,6 @@ sorted_arrays = bubble_sort(stargazers_count, project_name, project_url)
 # splitting bubble sort output into the original three arrays then slicing the arrays to only contain the first 10
 # values in each
 stargazers_count, project_name, project_url = sorted_arrays[0][0:10], sorted_arrays[1][0:10], sorted_arrays[2][0:10]
-print(stargazers_count, project_name, project_url)
 
 
 @app.route('/', methods=['GET'])
